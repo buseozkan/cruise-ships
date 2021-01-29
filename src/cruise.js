a93b47237f1bfe5bb3ec876d8fae5b7a2fed5d94
buseozkan <buse1.ozkan@gmail.com>
2021-01-29 23:01:54 +0200

    port

--- src/cruise.js
+++ src/cruise.js
@@ -1,11 +1,20 @@
 function Port(name) {
-    this.name = name;
+    this.name = name,
+    this.port = "Calais";
 }
 
-function Ship(portName) {
+function Ship (portName) {
   this.startingPort = portName;
 }
 
+Ship.prototype.setSail = function() {
+    this.startingPort = 0; // falsy
+}    
+
+Ship.prototype.dock = function (port) {
+
+    this.startingPort = port;
+}
 
 module.exports = {
     Ship,
--- tests/cruise.test.js
+++ tests/cruise.test.js
@@ -1,23 +1,33 @@
-//const Ship = require('../src/cruise')
-//
-//
-//describe('Ship', () => {
-//   it('can be instantiated', () => {
-//       expect(new Ship()).toBeInstanceOf(Object);
-//   });
-//})
-
-
 const {Ship, Port} = require('../src/cruise.js')
 
 
 describe('ship constructor', () => {
-    it('ship should be an instance of object', () => {
-        const port = new Port('Magusa Port');
+    test('ship should be an instance of object', () => {
+        const port = new Port('Dover Port');
         const ship = new Ship(port);
         expect(new Ship(port)).toBeInstanceOf(Object);
     });
 });
+
+describe('ship sail', () => {
+    test('can set sail', () => {
+        const ship = new Ship('Dover');
+        ship.setSail();
+        expect(ship.startingPort).toBeFalsy();
+    });
+});
+
+describe('dock from different port', () => {
+    test('can dock at a different port', () => {
+        const dover = new Port('Dover');
+        const ship = new Ship(dover);
+        const calais = new Port('Calais');
+        ship.dock(calais);
+
+        expect(ship.startingPort).toBe(calais);
+    });
+});
+
     
 
 
