9ace033c2c46c25f1a52ae40c662fec3a1ad5a18
buseozkan <buse1.ozkan@gmail.com>
2021-01-30 19:14:21 +0200

    step

--- tests/port.test.js
+++ tests/port.test.js
@@ -1,7 +1,15 @@
 const Port = require('../src/port.js');
 
 describe('Port', () => {
-    test('port can be initiated', () => {
-        expect(new Port()).toBeInstanceOf(Object);
+    test('port should be an instance of object', () => {
+        const port = new Port('Magusa Limani')
+        expect(new Port('Magusa Limani')).toBeInstanceOf(Object);
+    });
+});
+
+describe('name property', () => {
+    test('sets the name property', () => {
+        const port = new Port ('Magusa Limani');
+        expect(port.name).toEqual('Magusa Limani');
     });
 });
